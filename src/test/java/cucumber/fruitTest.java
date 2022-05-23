baea058eee86bced090b6efe9aa24f5c05820d28
gujjar0392 <gujjar0392@gmail.com>
2022-05-23 10:00:05 +0530

    first commit

---
+++ src/test/java/cucumber/fruitTest.java
@@ -0,0 +1,24 @@
+package cucumber;
+
+import org.testng.annotations.Test;
+
+public class fruitTest {
+	
+
+		  @Test
+		  public void tropicalTest() {
+			  System.out.println("pine apple comes under tropical");
+		  }
+
+		  @Test
+		  public void seasonalTest() {
+			  System.out.println("grapes are seasonal fruits");
+		  }
+
+		  @Test
+		  public void evergreenTest() {
+			  System.out.println("banana grows every season");
+		  }
+		}
+
+
---
+++ src/test/java/cucumber/seasons.java
@@ -0,0 +1,22 @@
+package cucumber;
+
+import org.testng.annotations.Test;
+
+public class seasons {
+	  @Test
+	  public void summer() {
+		  System.out.println("weather is hot in this season");
+	  }
+
+	  @Test
+	  public void winter() {
+		  System.out.println("weather is cold in this season");
+	  }
+
+	  @Test
+	  public void monsoon() {
+		  System.out.println("weather is rainy in this season");
+	  }
+	}
+
+
